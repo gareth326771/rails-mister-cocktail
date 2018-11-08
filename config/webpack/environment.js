3e8e9bc57d2709e571917adac7b65cc0458dc18d
Gareth Phillips <gareth326771@hotmail.com>
2018-11-08 12:52:51 +0000

    app set up

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+module.exports = environmentconst webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+)
+module.exports = environment
