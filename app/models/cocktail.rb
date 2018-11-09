e47b2ea5b82c821ed7ba6c51e35b72faab244a35
Gareth Phillips <gareth326771@hotmail.com>
2018-11-09 23:02:20 +0000

    app with some css

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,13 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+
+  def cocktail_difficulty(cocktail)
+    if cocktail.doses.count >= 3
+      @difficulty == "HARD"
+    elsif cocktail.doses.count == 2
+      @difficulty == "MEDIUM"
+    elsif cocktail.doses.count <= 1
+      @difficulty == "EASY"
+    end
 end
